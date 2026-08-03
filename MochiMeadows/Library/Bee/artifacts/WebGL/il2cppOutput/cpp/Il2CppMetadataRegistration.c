0ceceacd062ce14dc807bbff29dcbe19b44f120b
Greg Nazario <greg@gnazar.io>
2026-08-03 13:13:43 -0700

    WebGL build (GitHub Pages)

---
+++ MochiMeadows/Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Il2CppMetadataRegistration.c
@@ -0,0 +1,36 @@
+﻿#include "pch-c.h"
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern Il2CppGenericClass* const g_Il2CppGenericTypes[];
+extern const Il2CppGenericInst* const g_Il2CppGenericInstTable[];
+extern const Il2CppGenericMethodFunctionsDefinitions g_Il2CppGenericMethodFunctions[];
+extern const Il2CppType* const  g_Il2CppTypeTable[];
+extern const Il2CppMethodSpec g_Il2CppMethodSpecTable[];
+extern const int32_t* g_FieldOffsetTable[];
+extern const Il2CppTypeDefinitionSizes* g_Il2CppTypeDefinitionSizesTable[];
+IL2CPP_EXTERN_C const Il2CppMetadataRegistration g_MetadataRegistration;
+const Il2CppMetadataRegistration g_MetadataRegistration = 
+{
+	5550,
+	g_Il2CppGenericTypes,
+	3632,
+	g_Il2CppGenericInstTable,
+	50399,
+	g_Il2CppGenericMethodFunctions,
+	16489,
+	g_Il2CppTypeTable,
+	58540,
+	g_Il2CppMethodSpecTable,
+	4853,
+	g_FieldOffsetTable,
+	4853,
+	g_Il2CppTypeDefinitionSizesTable,
+	0,
+	NULL,
+};
---
+++ MochiMeadows/Library/Bee/artifacts/WebGL/il2cppOutput/cpp/UnityEngine.ScreenCaptureModule_CodeGen.c
@@ -0,0 +1,51 @@
+﻿#include "pch-c.h"
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void EmbeddedAttribute__ctor_m3CC7FEB78C868005AE22363D8099E557C2B92C68 (void);
+extern void RefSafetyRulesAttribute__ctor_m086215480C500C2C07DD6DE0E5D00E54F810786E (void);
+extern void ScreenCapture_CaptureScreenshot_m897B0264A202824D377CCD9A5221B164DE2CF9DE (void);
+extern void ScreenCapture_CaptureScreenshot_m35F14D593665968FE8F449104B66CE35EB22344D (void);
+extern void ScreenCapture_CaptureScreenshot_Injected_m3E20317C5BEC5A7B4D7999B677345256C5B28A4F (void);
+static Il2CppMethodPointer s_methodPointers[5] = 
+{
+	EmbeddedAttribute__ctor_m3CC7FEB78C868005AE22363D8099E557C2B92C68,
+	RefSafetyRulesAttribute__ctor_m086215480C500C2C07DD6DE0E5D00E54F810786E,
+	ScreenCapture_CaptureScreenshot_m897B0264A202824D377CCD9A5221B164DE2CF9DE,
+	ScreenCapture_CaptureScreenshot_m35F14D593665968FE8F449104B66CE35EB22344D,
+	ScreenCapture_CaptureScreenshot_Injected_m3E20317C5BEC5A7B4D7999B677345256C5B28A4F,
+};
+static const int32_t s_InvokerIndices[5] = 
+{
+	5680,
+	4349,
+	7860,
+	6827,
+	6827,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_UnityEngine_ScreenCaptureModule_CodeGenModule;
+const Il2CppCodeGenModule g_UnityEngine_ScreenCaptureModule_CodeGenModule = 
+{
+	"UnityEngine.ScreenCaptureModule.dll",
+	5,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
